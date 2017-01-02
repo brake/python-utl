c9171a9e3e81ae0ba04222b804263370f09f79d4
Constantin Roganov <rccbox@gmail.com>
2017-01-03 02:07:37 +0300

    [added] license text [removed] imports of all submodules

--- __init__.py
+++ __init__.py
@@ -1,23 +1,36 @@
-﻿# -*- coding: UTF-8 -*-
-#-------------------------------------------------------------------------------
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# ------------------------------------------------------------------------------
 # Name:    __init__.py
 # Package: utl
 # Project: utl
 #
 # Created: 25.03.13 17:54
-# Copyright:  (c) Constantin Roganov, 2013
-# Licence:    The MIT License
-#-------------------------------------------------------------------------------
-#!/usr/bin/env python
+# Copyright 2013-2016 © Constantin Roganov
+# License: The MIT License
+# ------------------------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
 
 """Utilities I use in many of my projects"""
 
+from future.builtins.disabled import file
+
 __author__ = 'Constantin Roganov'
 
-
-from .file import *
-from .text import *
-from .version import *
-from .hex import *
-from .misc import *
-from .wx_ import *
