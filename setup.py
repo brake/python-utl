b4e988ea54ceb804c61f79937e400b3472e7fff3
Constantin Roganov <rccbox@gmail.com>
2017-01-03 22:51:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,8 +41,8 @@ install_requires = ['future >= 0.16.0'] if sys.version_info[0] == 2 else []
 setup(
     name='utl',
     version=__version__,
-    packages=find_packages(),
-    py_modules=['text', 'file', 'wx_', 'hex', 'misc', 'version'],
+    packages=[utl],
+#    py_modules=['text', 'file', 'wx_', 'hex', 'misc', 'version'],
     install_requires=install_requires,
     author=__author__,
     author_email='rccbox at gmail dot com',
